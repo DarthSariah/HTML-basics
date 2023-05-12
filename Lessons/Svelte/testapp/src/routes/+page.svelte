50d7ef789f8f78f9db9f5a58a3268623a9bfbdc8
Andra Toterman <darthsariah11@gmail.com>
2023-05-12 23:03:27 +0300

    svelte

---
+++ Lessons/Svelte/testapp/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	let text = '';
+</script>
+
+<h1>Insert text</h1>
+
+<input type="text" bind:value={text} />
+
+<div>
+	{text.toUpperCase()}
+</div>
+
+<style>
+	div {
+		margin-top: 15px;
+	}
+</style>
