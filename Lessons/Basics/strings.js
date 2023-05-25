488473bb97b491d1ae7b6e0eb76c480774e79691
Stathis Stathopoulos <stathopoulos.stathis98@gmail.com>
2023-05-25 20:03:23 +0300

    Version one

---
+++ Lessons/Basics/strings.js
@@ -0,0 +1,20 @@
+/**
+ * Strings are a data type used to represent text and are wrapped in either single or double quotes.
+ */
+let firstName = "John";
+let lastName = "Doe";
+
+/**
+ * You can use the + operator to concatenate (join) two strings together.
+ */
+let fullName = firstName + " " + lastName;
+console.log(fullName); // John Doe
+
+/**
+ * You can use the += operator to concatenate (join) two strings together.
+ * The += operator adds the value of the right operand to a variable and assigns the result to the variable.
+ * Example: x += y is the same as x = x + y
+ */
+let fullName2 = "John";
+fullName2 += " Doe";
+console.log(fullName2); // John Doe
