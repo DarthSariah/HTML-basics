b017ce2060dd6faa5d7b4d311c2789feef114231
Andra Toterman <darthsariah11@gmail.com>
2023-05-28 17:28:40 +0300

    Co-authored-by: Stathis Stathopoulos <stathis1998@users.noreply.github.com>

--- LeetCode/11_except.js
+++
@@ -1,8 +0,0 @@
-/**
- * @param {string} val
- * @return {Object}
- */
-var expect = function (val) {};
-
-console.log(expect(5).toBe(5)); // {"value": true}
-console.log(expect(5).notToBe(5)); // {"error": "Not Equal"}
