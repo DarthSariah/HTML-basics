f34022887ff58004ba658da838186260569f25b9
Andra Toterman <darthsariah11@gmail.com>
2023-05-28 21:46:31 +0300

    js exercises

---
+++ JS_Exercises/05_isPrime.js
@@ -0,0 +1,12 @@
+function isPrime(num) {
+  if (num === 1) return false;
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+console.log(isPrime(1)); // false
+console.log(isPrime(2)); // true
+console.log(isPrime(7)); // true
+console.log(isPrime(10)); // false
---
+++ JS_Exercises/06_findFactorial.js
@@ -0,0 +1,7 @@
+function findFactorial(num) {
+  // Factorial of 5 = 5 * 4 * 3 * 2 * 1
+  for (let i = 1; i < num; i++) {}
+}
+
+console.log(findFactorial(5)); // 120
+console.log(findFactorial(10)); // 3628800
