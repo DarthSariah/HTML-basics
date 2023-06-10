57dd1fc30bdfacaa6b7756bc205eb3e7147ddb4a
Andra Toterman <darthsariah11@gmail.com>
2023-06-10 17:07:27 +0300

    Co-authored-by: Stathis Stathopoulos <stathis1998@users.noreply.github.com>

--- JS_Exercises/06_findFactorial.js
+++ JS_Exercises/06_findFactorial.js
@@ -1,7 +1,10 @@
 function findFactorial(num) {
   // Factorial of 5 = 5 * 4 * 3 * 2 * 1
-  for (let i = 1; i < num; i++) {}
+  let result = 1;
+  for (let i = 2; i <= num; i++) {
+    result *= i; // result = result * i
+  }
+  return result;
 }
 
 console.log(findFactorial(5)); // 120
-console.log(findFactorial(10)); // 3628800
