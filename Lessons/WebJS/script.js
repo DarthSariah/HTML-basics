b64f7de6bdca247cc24ffb9f95b23c00852c0edd
Andra Toterman <darthsariah11@gmail.com>
2023-06-10 20:49:08 +0300

    chessboard

--- Lessons/WebJS/script.js
+++ Lessons/WebJS/script.js
@@ -24,8 +24,45 @@ for (let i = 0; i < books.length; i++) {
 // ------------------------------
 
 const colorSelect = document.querySelector("#color-select");
-
 colorSelect.addEventListener("change", function (event) {
-  const color = event.target.value; // The value of the selected option
-  booksDiv.style.backgroundColor = color; // Change the background color of the div
+  const root = document.querySelector(":root");
+  root.style.setProperty("--color2", event.target.value);
 });
+
+// ------------------------------
+// 1. Select the div of the chess board
+// 2. Create a loop that runs 64 times (8 rows, 8 columns)
+// 3. Create a new div element
+// 4. Add the class "box" to the div and "black" or "white" depending on the row and column
+// 5. Append the div to the chess board
+
+// ex. element.classList.add("class-name");
+
+const chessBoardDiv = document.querySelector("#chess-board");
+
+let isRowEven = true;
+let boxNumber = 9;
+for (let i = 0; i < 64; i++) {
+  if (i % 8 === 0) {
+    isRowEven = !isRowEven;
+    boxNumber--;
+  }
+
+  const div = document.createElement("div");
+  div.classList.add("box");
+  const letter = String.fromCharCode(65 + (i % 8));
+  div.textContent = letter + String(boxNumber);
+
+  // div.classList.add(isRowEven ^ (i % 2 !== 0) ? "black" : "white");
+  if ((i % 2 === 0) ^ isRowEven) {
+    // XOR
+    //(if both conditions are false then the result is true)
+    //(if both conditions are true then the result is false)
+
+    div.classList.add("white");
+  } else {
+    div.classList.add("black");
+  }
+
+  chessBoardDiv.appendChild(div);
+}
